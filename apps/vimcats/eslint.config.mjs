70666aa3cfd1f2ae62646c5aa252d2b07c51c7fb
Brian Spragge <brianspragge@gmail.com>
2026-08-30 09:35:54 -0700

    Add button component styles and behaviors

--- apps/vimcats/eslint.config.mjs
+++ apps/vimcats/eslint.config.mjs
@@ -1,18 +1,24 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import { config as baseConfig } from '@repo/eslint-config/base'
+import { nextJsConfig } from '@repo/eslint-config/next-js'
+import onlyWarn from 'eslint-plugin-only-warn'
+import turboPlugin from 'eslint-plugin-turbo'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...baseConfig,
+  ...nextJsConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
